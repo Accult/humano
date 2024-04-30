24557ac1cb0d30cb17f258b6f87bc350faf7ddcc
accult <acculltt@gmail.com>
2024-04-30 13:23:24 +0300

    fix(telnyx-and-ghl):

--- webhook_receiver.py
+++ webhook_receiver.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import ghl_api
 
 app = Flask(__name__)
@@ -6,50 +6,49 @@ app = Flask(__name__)
 
 @app.route("/api/v1/telnyx-response", methods=["POST"])
 def webhook():
-    # Отримуємо дані з вебхуку
-    # data = request.json
-    data = [
-        {
-            "data": {
-                "event_type": "message.finalized",
-                "id": "e8b33d68-e65c-4394-852d-bc495d87d005",
-                "occurred_at": "2024-04-29T10:17:42.574+00:00",
-                "payload": {
-                    "cc": [],
-                    "completed_at": "2024-04-29T10:17:42.574+00:00",
-                    "cost": {"amount": "0.0040", "currency": "USD"},
-                    "direction": "outbound",
-                    "encoding": "GSM-7",
-                    "errors": [],
-                    "from": {"carrier": "Telnyx", "line_type": "Wireless", "phone_number": "+19012375438"},
-                    "id": "40318f29-5c3f-4689-aa8a-4f41c6d49178",
-                    "media": [],
-                    "messaging_profile_id": "40018f15-e2f3-4ec6-8390-c9ed22cb5605",
-                    "organization_id": "2ea3868b-fef8-452f-8d8b-fb5ceb0e4c78",
-                    "parts": 1,
-                    "received_at": "2024-04-29T10:17:42.515+00:00",
-                    "record_type": "message",
-                    "sent_at": "2024-04-29T10:17:42.552+00:00",
-                    "tags": [],
-                    "text": "Hi, nice to meet you!",
-                    "to": [
-                        {
-                            "carrier": "TELNYX LLC",
-                            "line_type": "Pre-Paid Wireless",
-                            "phone_number": "+19097688030",
-                            "status": "delivered",
-                        }
-                    ],
-                    "type": "SMS",
-                    "valid_until": "2024-04-29T11:17:42.515+00:00",
-                    "webhook_failover_url": "https://hook.us1.make.com/f77apspzg119xvp7cyb9tv4v0m1tdd8n",
-                    "webhook_url": "",
-                },
-                "record_type": "event",
-            },
-            "meta": {"attempt": 1, "delivered_to": "https://hook.us1.make.com/f77apspzg119xvp7cyb9tv4v0m1tdd8n"},
-        }
-    ]
+    data = request.json
+    # data = [
+    #     {
+    #         "data": {
+    #             "event_type": "message.finalized",
+    #             "id": "e8b33d68-e65c-4394-852d-bc495d87d005",
+    #             "occurred_at": "2024-04-29T10:17:42.574+00:00",
+    #             "payload": {
+    #                 "cc": [],
+    #                 "completed_at": "2024-04-29T10:17:42.574+00:00",
+    #                 "cost": {"amount": "0.0040", "currency": "USD"},
+    #                 "direction": "outbound",
+    #                 "encoding": "GSM-7",
+    #                 "errors": [],
+    #                 "from": {"carrier": "Telnyx", "line_type": "Wireless", "phone_number": "+19012375438"},
+    #                 "id": "40318f29-5c3f-4689-aa8a-4f41c6d49178",
+    #                 "media": [],
+    #                 "messaging_profile_id": "40018f15-e2f3-4ec6-8390-c9ed22cb5605",
+    #                 "organization_id": "2ea3868b-fef8-452f-8d8b-fb5ceb0e4c78",
+    #                 "parts": 1,
+    #                 "received_at": "2024-04-29T10:17:42.515+00:00",
+    #                 "record_type": "message",
+    #                 "sent_at": "2024-04-29T10:17:42.552+00:00",
+    #                 "tags": [],
+    #                 "text": "Hi, nice to meet you!",
+    #                 "to": [
+    #                     {
+    #                         "carrier": "TELNYX LLC",
+    #                         "line_type": "Pre-Paid Wireless",
+    #                         "phone_number": "+19097688030",
+    #                         "status": "delivered",
+    #                     }
+    #                 ],
+    #                 "type": "SMS",
+    #                 "valid_until": "2024-04-29T11:17:42.515+00:00",
+    #                 "webhook_failover_url": "https://hook.us1.make.com/f77apspzg119xvp7cyb9tv4v0m1tdd8n",
+    #                 "webhook_url": "",
+    #             },
+    #             "record_type": "event",
+    #         },
+    #         "meta": {"attempt": 1, "delivered_to": "https://hook.us1.make.com/f77apspzg119xvp7cyb9tv4v0m1tdd8n"},
+    #     }
+    # ]
 
     sender_phone = data[0]["data"]["payload"]["from"]["phone_number"]
     message = data[0]["data"]["payload"]["text"]
